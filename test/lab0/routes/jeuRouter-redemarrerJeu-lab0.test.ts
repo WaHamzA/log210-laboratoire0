86580d0fe32fbd0b5af5842d01408d2f9a7538ef
WaHamzA <wahidihamza@hotmail.com>
2024-05-28 22:17:57 -0400

    3. Écrire des tests pour la fonctionnalité
    -describe
    -beforeAll
    -it

--- test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,36 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import { Joueur } from '../../../src/core/Joueur';
+import request from 'supertest';
+import app from '../../../src/app';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("should implement test", async () => {
-    throw new Error("Ce test n'a pas été défini")
-  });
+
+let j1;
+let j2;
+
+beforeAll(async () =>{
+  j1 = new Joueur("bob");
+  j2 = new Joueur("karl");
+
+
 });
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it('should return status 200 and JSON response', async () => {
+    const response = await request(app)
+      .get('/api/v1/jeu/redemarrerJeu')
+      .expect('Content-Type', /json/)
+      .expect(200);
+  });
+
+  it('should have no player left', () =>{
+    expect(j1==null && j2==null ).toBeTruthy();
+  });
+
+});
+
+
+
+
+
